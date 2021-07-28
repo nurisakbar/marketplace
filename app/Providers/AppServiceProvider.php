c898559c4b7e65dbed958e8d5bca65a04c188b31
rgrahardi <rgrahardi2019@gmail.com>
2021-07-28 19:54:44 +0700

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,5 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    public function boot()
+    {
+    Schema::defaultStringLength(191);
     }
 }
