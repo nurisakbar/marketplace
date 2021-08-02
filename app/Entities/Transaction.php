33f0edaaa488eb38d0b513b5bfc4c9f06e54a38f
Siti Laelatur Rochmah <lelaela6@gmail.com>
2021-08-02 12:51:06 +0700

    Add migration & model for module-transaction

---
+++ app/Entities/Transaction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Transaction.
+ *
+ * @package namespace App\Entities;
+ */
+class Transaction extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'store_id',
+        'courier_service_id',
+        'user_address_id',
+        'status',
+        'note'
+    ];
+}
---
+++ app/Repositories/TransactionRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories;
+
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+/**
+ * Interface TransactionRepository.
+ *
+ * @package namespace App\Repositories;
+ */
+interface TransactionRepository extends RepositoryInterface
+{
+    //
+}
