75f67a330e9408a16e9f563f9f4f5f168707c6af
Nuris Akbar <nuris.akbar@gmail.com>
2021-08-02 16:43:13 +0700

    implement ApiResponseTrait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use App\Http\Controllers\ApiResponseTrait;
 
 class Controller extends BaseController
 {
-    //
+    use ApiResponseTrait;
 }
